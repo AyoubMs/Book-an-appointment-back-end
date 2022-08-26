862215710b0d6ed3373ce986c0b8b37cc5b3c54d
AyoubMS <chahayb563@gmail.com>
2022-08-26 15:55:28 +0100

    add secret_key_base

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   validates :name, uniqueness: true, presence: true
 
   def generate_jwt
-    JWT.encode({ id:, exp: 60.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
+    JWT.encode({ id:, exp: 60.days.from_now.to_i }, ENV['SECRET_KEY_BASE'])
   end
 end
